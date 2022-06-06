cee985869b31e00636d6718e04c0631c1cae2564
meta-ela <anxhelameta@live.it>
2022-06-06 15:14:34 +0200

    Adds array of objects

---
+++ snack1/snack1.js
@@ -0,0 +1,31 @@
+/*
+Snack 1
+Creare un array di oggetti:
+Ogni oggetto descriverà una bici da corsa con le seguenti proprietà: nome e peso.
+Stampare a schermo la bici con peso minore utilizzando destructuring e template literal
+
+*/
+
+// creare array di oggetti
+const bikesList = [
+    {
+        name: "Willer 0 SLR",
+        weight: "6.5"
+    },
+    {
+        name: "Cannondale SuperSix",
+        weight: "8.0"
+    },
+    {
+        name: "Trek Emonda 2020",
+        weight: "6.81"
+    },
+    {
+        name: "Trek Emonda SLR",
+        weight: "4.65"
+    },
+    {
+        name: "SAVA Phantom 700C",
+        weight: "8.1"
+    },
+];
