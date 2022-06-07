adba3f57063d834304a5330899b9c9a7787e47bb
meta-ela <anxhelameta@live.it>
2022-06-07 17:29:33 +0200

    Adds snack 3 and 4 not solved

---
+++ snack3/snack3.js
@@ -0,0 +1,40 @@
+/*
+Snack 3
+Dato l’array di nomi:
+const myArray = [‘Michele’, ‘Fabio’, ‘Roberto’, ‘Giovanni’, ‘Simone’, ‘Chiara’];
+e dati due numeri min e max (min più piccolo di max).
+
+Creiamo un nuovo array contenente i valori che hanno la posizione compresa tra i due numeri.
+Eseguiamo questo esercizio prima con forEach e poi con filter.
+*/
+
+
+const myArray = [
+    "Michele", 
+    "Fabio", 
+    "Roberto", 
+    "Giovanni", 
+    "Simone", 
+    "Chiara"
+];
+
+
+
+
+myArray.forEach((name, index) => {
+    console.log(name, index);
+
+    let newArray = [];
+    /* if (name >= index[1] && name <= index[4]) {
+        return name;
+        newArray.push(name, index);
+    } */
+
+    
+    
+});
+
+
+/* let arrayOnlySavedName = myArray.filter(()) => {
+
+} */
