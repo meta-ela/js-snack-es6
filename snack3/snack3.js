085f7a20496e26810763e75f4c30a9624d2b3f23
meta-ela <anxhelameta@live.it>
2022-06-08 09:54:00 +0200

    Adds snack 3 live correction

--- snack3/snack3.js
+++ snack3/snack3.js
@@ -10,31 +10,47 @@ Eseguiamo questo esercizio prima con forEach e poi con filter.
 
 
 const myArray = [
-    "Michele", 
-    "Fabio", 
-    "Roberto", 
-    "Giovanni", 
-    "Simone", 
-    "Chiara"
+    "Michele",  /* 0 */
+    "Fabio",    /* 1 */
+    "Roberto",  /* 2 */
+    "Giovanni", /* 3 */ 
+    "Simone",   /* 4 */ 
+    "Chiara"    /* 5 */
 ];
 
+console.table(myArray);
+
+let min = 1;
+let max = 4;
+
+let newArray = [];
 
 
+// FOREACH ////////////////////////////////////////////
 
-myArray.forEach((name, index) => {
-    console.log(name, index);
-
-    let newArray = [];
-    /* if (name >= index[1] && name <= index[4]) {
-        return name;
-        newArray.push(name, index);
-    } */
-
-    
-    
+myArray.forEach((name, i) => {
+    // se l'indice è compreso tra min e max allora pushare nell'array vuoto
+    if (i >= min && i <= max) {
+        newArray.push(name);
+    }
 });
 
+console.log(newArray);
 
-/* let arrayOnlySavedName = myArray.filter(()) => {
 
-} */
+
+
+// FILTER ////////////////////////////////////////////
+
+/* let arrayOnlySavedName = myArray.filter((name, i) => {
+    // per essere pushato nell'array che crea in automatico filter
+    // il return deve risultare true
+    if (i >= min && i <= max) {
+        return true
+    }
+}); */
+
+// versione ridotta
+let arrayOnlySavedName = myArray.filter((name, i) => i >= min && i <= max);
+
+console.log(arrayOnlySavedName);
